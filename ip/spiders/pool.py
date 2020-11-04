dc758545d44db9c3aeae22c40fdf914c48b9fae5
summer250 <1667162831@qq.com>
2020-11-04 10:40:07 +0800

    第一次更新

--- ip/spiders/pool.py
+++ ip/spiders/pool.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from ip.items import IpItem
-
+import requests
+requests.packages.urllib3.disable_warnings()
 
 class PoolSpider(scrapy.Spider):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
+    }
     name = 'pool'
     allowed_domains = ['ip.jiangxianli.com']
+    test_url = 'https://www.baidu.com/'
     start_urls = ['https://ip.jiangxianli.com/?page=1&country=%E4%B8%AD%E5%9B%BD']
 
     def parse(self, response):
@@ -20,45 +25,40 @@ class PoolSpider(scrapy.Spider):
 
             # ip地址
             ip = tr.css('td:nth-child(1)::text').get()
-            item['ip'] = ip
 
             # 端口号
             port = tr.css('td:nth-child(2)::text').get()
-            item['port'] = port
 
             # 匿名度
             anons = tr.css('td:nth-child(3)::text').get()
-            item['anons'] = anons
 
             # 类型：http/https
             schema = tr.css('td:nth-child(4)::text').get()
-            item['schema'] = schema
 
             # 国家
             position = tr.css('td:nth-child(5)::text').get()
-            item['position'] = position
 
             # 位置
             country = tr.css('td:nth-child(6)::text').get()
-            item['country'] = country
 
             # 响应速度
             speed = tr.css('td:nth-child(8)::text').get()
-            item['schema'] = schema
 
             # 最后验证时间
             verify_time = tr.css('td:nth-child(10)::text').get()
-            item['verify_time'] = verify_time
 
+            # 测试ip可用性
+            proxies = {'http': 'https://' + ip+':'+port}
+            response1 = requests.get(url=self.test_url, headers=self.headers, proxies=proxies)
+            if response1.status_code == 200:
+                item['ip'] = ip
+                item['port'] = port
+                item['anons'] = anons
+                item['schema'] = schema
+                item['position'] = position
+                item['country'] = country
+                item['speed'] = speed
+                item['verify_time'] = verify_time
 
-
-
-
-
-
-
-
-            item['speed'] = speed
-
-            print(item)
-            yield item
+                print(item)
+                yield item
